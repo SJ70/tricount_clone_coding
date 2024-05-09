70b224fd7c40320e0302fb65fe125f8a8ac01a76
SJ70 <seojae70@gmail.com>
2024-05-09 18:21:29 +0900

    spring boot

---
+++ src/main/java/com/example/tricount/TricountCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tricount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricountCloneCodingApplication.class, args);
+	}
+
+}
