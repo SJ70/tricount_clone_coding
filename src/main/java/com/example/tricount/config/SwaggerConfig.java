6f849d10b248be38a0664af3946c6603f3191f10
SJ70 <seojae70@gmail.com>
2024-05-10 23:29:54 +0900

    feat: SwaggerConfig 클래스 작성
    - 정보 수정
    - 토큰 인증 적용

---
+++ src/main/java/com/example/tricount/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.tricount.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Tricount 클론코딩 API 명세서",
+                description = "구름톤 트레이닝 풀스택 과제 12",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
+}
