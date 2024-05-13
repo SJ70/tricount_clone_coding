feb2776e19d6e83761344565568fc9991ab76e26
SJ70 <seojae70@gmail.com>
2024-05-14 06:31:14 +0900

    fix: 잘못된 연관관계 수정
    - 1:1 이 아닌 1:N

--- src/main/java/com/example/tricount/entity/AmountPerMember.java
+++ src/main/java/com/example/tricount/entity/AmountPerMember.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +19,7 @@ public class AmountPerMember {
     @GeneratedValue
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     @JsonBackReference
     private Member member;
